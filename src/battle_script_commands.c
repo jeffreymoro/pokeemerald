f1fd776edc1b6c9f16c29c2a48473913f2ab9610
pokedev <test@test>
2021-07-14 15:25:55 +0000

    bump Lucky Egg to 250%

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -3332,7 +3332,7 @@ static void Cmd_getexp(void)
                     if (holdEffect == HOLD_EFFECT_EXP_SHARE)
                         gBattleMoveDamage += gExpShareExp;
                     if (holdEffect == HOLD_EFFECT_LUCKY_EGG)
-                        gBattleMoveDamage = (gBattleMoveDamage * 150) / 100;
+                        gBattleMoveDamage = (gBattleMoveDamage * 250) / 100;
                     if (gBattleTypeFlags & BATTLE_TYPE_TRAINER)
                         gBattleMoveDamage = (gBattleMoveDamage * 150) / 100;
 
